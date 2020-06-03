e90889403d8d45321e32a27c8aa661a4117c6853
ArthurNRL <anrl@hotmail.com>
2020-06-03 14:03:47 -0300

    S3 fix

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -150,4 +150,6 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGES = 'storages.backends.s3boto3.s3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_REGION_NAME = 'us-east-2' #change to your region
+AWS_S3_SIGNATURE_VERSION = 's3v4'
